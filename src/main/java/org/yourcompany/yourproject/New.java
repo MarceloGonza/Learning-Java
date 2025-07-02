eede3cc13d7c1ed3f6f409d667471da59838e9bd
Marcelo Gonzalez <marceloalbertogonzalez3@gmail.com>
2025-07-02 11:17:40 -0300

    Hello World!

---
+++ src/main/java/org/yourcompany/yourproject/New.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.yourcompany.yourproject;
+
+/**
+ *
+ * @author Marcelo
+ */
+public class New {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
