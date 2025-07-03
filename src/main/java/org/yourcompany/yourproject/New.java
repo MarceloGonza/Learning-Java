a21a268cc776384322e824be8a388e3d4318b626
Marcelo Gonzalez <marceloalbertogonzalez3@gmail.com>
2025-07-03 16:33:12 -0300

    Operadores aritméticos

--- src/main/java/org/yourcompany/yourproject/New.java
+++ src/main/java/org/yourcompany/yourproject/New.java
@@ -25,8 +25,8 @@ public class New {
         num1 = 4;
         num2 = 2;
 
-        resultado = num1 - num2;
-        System.out.println("El resultado de la resta es de: " + resultado);
+        resultado = num1 * num2;
+        System.out.println("El resultado de la multiplicacion es de: " + resultado);
     }
 }
 
