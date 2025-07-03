df0ba835187a67fb87bd5b97d5f300c7065e3f81
Marcelo Gonzalez <marceloalbertogonzalez3@gmail.com>
2025-07-03 16:32:18 -0300

    Operadores aritméticos#
    clear

--- src/main/java/org/yourcompany/yourproject/New.java
+++ src/main/java/org/yourcompany/yourproject/New.java
@@ -10,17 +10,31 @@ package org.yourcompany.yourproject;
  */
 public class New {
 
-    //public static void main(String[] args) {
-      //  System.out.println("Hello World!");
-   
-    
-    //}
     public static void main(String[] args) {
-        int numero = 10;
-        String nombre = "Gregoria";
-        char letra = 'B';
-        double temperatura = 2.1;
-        boolean siONo = true;
-        long largo = 123456789L;
+    System.out.println("Hello World!");
+   
+    //public static void main(String[] args) {
+      //Tipos de variables
+        //int numero = 10;
+        //String nombre = "Gregoria";
+        //char letra = 'B';
+        //double temperatura = 2.1;
+        //boolean siONo = false;
+        //long largo = 123456789L;
+        int num1, num2, resultado;
+        num1 = 4;
+        num2 = 2;
+
+        resultado = num1 - num2;
+        System.out.println("El resultado de la resta es de: " + resultado);
     }
 }
+
+//primero compilar con: javac src/main/java/org/yourcompany/yourproject/New.java
+//luego ejecutar con: java -cp src/main/java org.yourcompany.yourproject.New
+//si no ejecuta por muchos argumentos, usar comillas dobles
+//PASOS CON LOS QUE EJECUTE: 
+//cd "/d/Mis cosas/Programacion/Java/New"
+//javac src/main/java/org/yourcompany/yourproject/New.java
+//java -cp src/main/java org.yourcompany.yourproject.New
+// VOLVER A COMPILAR LA CLASE ANTES DE EJECUTAR PARAR QUE SE GUARDEN LOS CAMBIOS
