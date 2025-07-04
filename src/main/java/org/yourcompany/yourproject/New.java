32e9f08d80c32c1100c2c2e65a6f2bbec9e52258
Marcelo Gonzalez <marceloalbertogonzalez3@gmail.com>
2025-07-04 10:46:18 -0300

    Ejercicio intercambio de valores

--- src/main/java/org/yourcompany/yourproject/New.java
+++ src/main/java/org/yourcompany/yourproject/New.java
@@ -21,13 +21,40 @@ public class New {
         //double temperatura = 2.1;
         //boolean siONo = false;
         //long largo = 123456789L;
-        int num1, num2, resultado;
-        num1 = 4;
-        num2 = 2;
+        //int num1, num2, resultado;
+        //num1 = 4;
+        //num2 = 2;
 
-        resultado = num1 * num2;
-        System.out.println("El resultado de la multiplicacion es de: " + resultado);
-    }
+        //resultado = num1 * num2;
+        //System.out.println("El resultado de la multiplicacion es de: " + resultado);
+    
+
+    //EJERCICIO:
+    /*
+    Realizar un programa que permita el intercambio de valores entre dos variables. 
+    Por ejemplo: Si una variable número vale 35, y una variable número2 vale 20, 
+    realizar las acciones necesarias para que el número pase a valer 20 y númweo2 
+    pase a valer 35. Una vez realizado el cambio mostrar el resultado por pantalla.
+    */
+
+  int num1, num2, aux;
+
+  num1 = 35;
+  num2 = 20;
+
+  System.out.println("----Antes----");
+  System.out.println("Num1 = " + num1);
+  System.out.println("Num2 = " + num2);
+
+
+  aux = num2;//aux pasa a valer 20
+  num2 = num1;//num2 pasa a valer 35
+  num1 = aux;//num1 pasa a valer aux(20)
+
+  System.out.println("----Despues----");
+  System.out.println("Num1 = " + num1);
+  System.out.println("Num2 = " + num2);
+  }
 }
 
 //primero compilar con: javac src/main/java/org/yourcompany/yourproject/New.java
